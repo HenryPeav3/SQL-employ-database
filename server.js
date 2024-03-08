7c945396be89ee9e58336de39f4487314df4fb5b
Henry Peavler <henrypeavler@Henrys-MBP.attlocal.net>
2024-03-07 20:41:48 -0600

    added functions

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ function startApp() {
       });
   };
 
-
+// query database for all info on each table
   function viewDepartments(){
     connection.query("SELECT * FROM department", (err, res) =>{
         if(err) throw err;
@@ -75,9 +75,68 @@ function startApp() {
   }
 
 
+function addDepartment(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "departmentName",
+            message:"Please enter the name of the new department."
+        }
+    ]).then(function(answer){
+        connection.query("INSERT INTO department SET ?", {
+            name: answer.departmentName
+        })
+        console.log("Department added.")
+        startApp()
+    })
+}
+  
+// add role function
+function addRole() {
+    inquirer.prompt([
+        {
+          name: "title",
+          type: "input", 
+          message: "what is Department ID?"  
+        },
+        {
+          name: "salary",
+          type: "number",
+          message: "What is the salary of this position?"
+        },
+        {
+          name: "departmentId",
+          type: "rawlist",
+          message: "Select a department for this role"
+        },
+    ])
+}
 
+//add employee function
+function addEmployee() {
+    inquirer.prompt([
+        {
+            name: "firstName",
+            type: "input",
+            message: "What is the new emplyee's first name?"
+        },
+        {
+            name: "lastName",
+            type: "input",
+            message: "What is the new emplyee's last name?"
+        },
+        {
+            name: "roleId",
+            type: "rawlist",
+            message: "Select a role for the employee"
+        },
+        {
+            name: "managerId",
+            type: "rawlist",
+            message: "What is your managers roll?"
+        },
+    ])
+}
   
-  
-  
-  
+// update employee function
   startApp()
